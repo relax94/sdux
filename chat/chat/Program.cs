51685e6e7fe249c4153dedbe544a090bf237a5b7
reflex944 <relax944@gmail.com>
2015-01-18 21:55:55 +0200

    add socket server

---
+++ chat/chat/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace chat
+{
+    class Program
+    {
+
+        private static Socket _clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+       
+        private static void Connect()
+        {
+            int attempts = 0;
+
+            while(!_clientSocket.Connected)
+            {
+                try
+                {
+                    attempts++;
+                    _clientSocket.Connect(IPAddress.Loopback,51111);
+                }
+                catch(SocketException ex)
+                {
+                    Console.WriteLine(attempts);
+                }
+                
+            }
+            Console.Clear();
+            Console.WriteLine("Connected ");
+        }
+
+
+        private static void Send()
+        {
+
+            new Thread(() => {
+                while (true)
+                {
+                    byte[] recieveBuf = new byte[1024];
+                    int rec = _clientSocket.Receive(recieveBuf);
+                    byte[] data = new byte[rec];
+                    Array.Copy(recieveBuf, data, rec);
+                    Console.WriteLine();
+                    Console.WriteLine("Recieve : " + Encoding.ASCII.GetString(data));
+                    Console.WriteLine("\n Press enter to type your message \n");
+                }
+           
+            }).Start();
+
+            while(true)
+            {
+                
+                Console.Write("Enter message : ");
+                string req = Console.ReadLine();
+                byte[] buffer = Encoding.ASCII.GetBytes(req);
+                _clientSocket.Send(buffer);                            
+            }
+     
+        }
+        
+        static void Main(string[] args)
+        {
+           Connect();
+           Send();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ chat/socketserver/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Multi_Server
+{
+    //193.107.74.84
+    class Program
+    {
+        private static Socket _serverSocket;
+        private static readonly List<Socket> _clientSockets = new List<Socket>();
+        private const int _BUFFER_SIZE = 2048;
+        private const int _PORT = 51111;
+        private static readonly byte[] _buffer = new byte[_BUFFER_SIZE];
+
+        static void Main()
+        {
+            Console.Title = "Server";
+            SetupServer();
+            Console.ReadLine(); 
+            CloseAllSockets();
+        }
+
+        private static void SetupServer()
+        {
+            Console.WriteLine("Setting up server...");
+            _serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            _serverSocket.Bind(new IPEndPoint(IPAddress.Any, _PORT));
+            _serverSocket.Listen(5);
+            _serverSocket.BeginAccept(AcceptCallback, null);
+            Console.WriteLine("Server setup complete");
+        }
+
+        /// <summary>
+        /// Close all connected client (we do not need to shutdown the server socket as its connections
+        /// are already closed with the clients)
+        /// </summary>
+        private static void CloseAllSockets()
+        {
+            foreach (Socket socket in _clientSockets)
+            {
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+
+            _serverSocket.Close();
+        }
+
+        private static void AcceptCallback(IAsyncResult AR)
+        {
+            Socket socket;
+
+            try
+            {
+                socket = _serverSocket.EndAccept(AR);
+            }
+            catch (ObjectDisposedException) // I cannot seem to avoid this (on exit when properly closing sockets)
+            {
+                return;
+            }
+
+            _clientSockets.Add(socket);
+            socket.BeginReceive(_buffer, 0, _BUFFER_SIZE, SocketFlags.None, ReceiveCallback, socket);
+            Console.WriteLine("Client connected, waiting for request...");
+            _serverSocket.BeginAccept(AcceptCallback, null);
+        }
+
+        private static void ReceiveCallback(IAsyncResult AR)
+        {
+            Socket current = (Socket)AR.AsyncState;
+            int received;
+
+            try
+            {
+                received = current.EndReceive(AR);
+            }
+            catch (SocketException)
+            {
+                Console.WriteLine("Client forcefully disconnected");
+                current.Close(); // Dont shutdown because the socket may be disposed and its disconnected anyway
+                _clientSockets.Remove(current);
+                return;
+            }
+
+            byte[] recBuf = new byte[received];
+            Array.Copy(_buffer, recBuf, received);
+            string text = Encoding.ASCII.GetString(recBuf);
+            Console.WriteLine("Received Text: " + text);
+
+                byte[] data = Encoding.ASCII.GetBytes(text);
+                foreach (Socket imn in _clientSockets)
+                {
+                    if(imn != current)
+                         imn.Send(data);
+                }
+           
+
+            current.BeginReceive(_buffer, 0, _BUFFER_SIZE, SocketFlags.None, ReceiveCallback, current);
+        }
+    }
+}
